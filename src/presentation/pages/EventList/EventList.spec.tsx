16dec29e457ec109ba0f18ab79d172bdaf9b9477
niltonslf <niltonrck@gmail.com>
2022-12-10 10:35:00 -0300

    test: ensure EventList calls FetchEvent

--- src/presentation/pages/EventList/EventList.spec.tsx
+++ src/presentation/pages/EventList/EventList.spec.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { describe, test, expect } from 'vitest'
 
+import { EventModel } from '@/domain/models'
+import { mockEventListModel } from '@/domain/test/mock-fetch-event'
+import { FetchEvent } from '@/domain/usecases'
 import { theme } from '@/presentation/theme'
 import { ChakraProvider } from '@chakra-ui/react'
-import { render, screen } from '@testing-library/react'
+import { act, render, screen } from '@testing-library/react'
 
 import { EventList } from '.'
 
@@ -11,8 +14,27 @@ export const ThemeWrapper: React.FC<React.PropsWithChildren> = ({ children }) =>
   <ChakraProvider theme={theme}>{children}</ChakraProvider>
 )
 
-const makeSut = () => {
-  render(<EventList />, { wrapper: ThemeWrapper })
+class FetchEventSpy implements FetchEvent {
+  callsCount: number = 0
+
+  async fetchAll(): Promise<EventModel[]> {
+    this.callsCount++
+    return mockEventListModel()
+  }
+}
+
+type SutTypes = {
+  fetchEventSpy: FetchEventSpy
+}
+
+const makeSut = (): SutTypes => {
+  const fetchEventSpy = new FetchEventSpy()
+
+  render(<EventList fetchEvent={fetchEventSpy} />, { wrapper: ThemeWrapper })
+
+  return {
+    fetchEventSpy,
+  }
 }
 
 describe('EventList Page', () => {
@@ -23,5 +45,16 @@ describe('EventList Page', () => {
 
     expect(eventList.querySelectorAll("[data-testid='event-skeleton']").length).toBe(6)
     expect(eventList.querySelectorAll("[data-testid='event-item']").length).toBe(0)
+  })
+
+  test('should call fetchEvent', async () => {
+    const { fetchEventSpy } = makeSut()
+    const eventList = screen.getByTestId('event-list')
+
+    await act(async () => eventList)
+
+    expect(eventList.querySelectorAll("[data-testid='event-skeleton']").length).toBe(0)
+    expect(screen.queryAllByTestId('event-item')).toHaveLength(2)
+    expect(fetchEventSpy.callsCount).toBe(1)
   })
 })
