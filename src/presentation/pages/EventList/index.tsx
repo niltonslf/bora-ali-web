844ee9f9d77eddd8778af61745ba1d010bf87b4e
niltonslf <niltonrck@gmail.com>
2022-12-12 08:20:01 -0300

    feat: ensure EventList show error on fail

--- src/presentation/pages/EventList/index.tsx
+++ src/presentation/pages/EventList/index.tsx
@@ -5,7 +5,7 @@ import { EventModel } from '@/domain/models'
 import { FetchEvent } from '@/domain/usecases'
 import { EventCard, Header } from '@/presentation/components'
 import { EventCardSkeleton } from '@/presentation/components/EventCard/EventCardSkeleton'
-import { Flex, Grid, Box } from '@chakra-ui/react'
+import { Flex, Grid, Box, Heading } from '@chakra-ui/react'
 import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api'
 
 type EventListProps = {
@@ -15,6 +15,7 @@ type EventListProps = {
 export const EventList: React.FC<EventListProps> = ({ fetchEvent }) => {
   const [events, setEvents] = useState<EventModel[]>([])
   const [coords, setCoords] = useState({ lat: -33.91519386250274, lng: 18.420095308767127 })
+  const [error, setError] = useState(null)
   const [, setMap] = useState(null)
 
   const { isLoaded } = useJsApiLoader({
@@ -31,9 +32,10 @@ export const EventList: React.FC<EventListProps> = ({ fetchEvent }) => {
   }, [])
 
   useEffect(() => {
-    fetchEvent.fetchAll().then((events) => {
-      setEvents(events)
-    })
+    fetchEvent
+      .fetchAll()
+      .then((events) => setEvents(events))
+      .catch((error) => setError(error.message))
   }, [])
 
   useEffect(() => {
@@ -50,21 +52,22 @@ export const EventList: React.FC<EventListProps> = ({ fetchEvent }) => {
     <Grid minHeight='100vh' width='100%' gridTemplateRows='80px auto' position='relative'>
       <Header />
       <Flex position='relative' width='100%'>
-        <Grid
-          flex={1}
-          gridTemplateColumns='1fr 1fr'
-          background='white'
-          height='100%'
-          padding='1rem'
-          gap={4}
-          data-testid='event-list'
-        >
-          {events.length ? (
-            events.map((event, index) => <EventCard event={event} key={index} />)
+        <Flex flex={1} background='white' height='100%' padding='1rem'>
+          <Heading textStyle='h1' data-testid='title'>
+            Events found
+          </Heading>
+          {error ? (
+            <Flex data-testid='error'>{error}</Flex>
           ) : (
-            <EventCardSkeleton />
+            <Grid gridTemplateColumns='1fr 1fr' gap={4} data-testid='event-list'>
+              {events.length ? (
+                events.map((event, index) => <EventCard event={event} key={index} />)
+              ) : (
+                <EventCardSkeleton />
+              )}
+            </Grid>
           )}
-        </Grid>
+        </Flex>
         <Box flex={1.5} height='calc(100vh - 80px)' position='sticky' top='80px'>
           {isLoaded && (
             <GoogleMap
