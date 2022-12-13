d9e3b2d2cef1d4b9a89dc0d2186bfb21bb8def5b
niltonslf <niltonrck@gmail.com>
2022-12-13 14:56:54 -0300

    refactor: move sut creation to a factory method

--- src/presentation/pages/Login/login.spec.tsx
+++ src/presentation/pages/Login/login.spec.tsx
@@ -1,4 +1,4 @@
-import { createMemoryHistory } from 'history'
+import { createMemoryHistory, MemoryHistory } from 'history'
 import { Router } from 'react-router-dom'
 import { describe, expect, test } from 'vitest'
 
@@ -7,18 +7,28 @@ import { render, screen } from '@testing-library/react'
 
 import { Login } from '.'
 
-const history = createMemoryHistory()
+type SutTypes = {
+  history: MemoryHistory
+}
+
+const makeSut = (): SutTypes => {
+  const history = createMemoryHistory()
+  render(
+    <Router location={history.location} navigator={history}>
+      <Login />
+    </Router>,
+    { wrapper: ThemeWrapper }
+  )
+
+  return {
+    history,
+  }
+}
 
 describe('Login', () => {
   test('should render Login ', () => {
-    render(
-      <Router location={history.location} navigator={history}>
-        <Login />
-      </Router>,
-      {
-        wrapper: ThemeWrapper,
-      }
-    )
+    makeSut()
+
     const googleButton = screen.getByTestId('google-button')
 
     expect(googleButton).toBeInTheDocument()
