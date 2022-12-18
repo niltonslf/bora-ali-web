98380aa67411f2d68b4f278ac1f638d0ad7c1d55
niltonslf <niltonrck@gmail.com>
2022-12-18 07:11:43 -0300

    test: ensure RemoteCreateUser throws UnexpectedError on fail

--- src/data/usecases/create-user/remote-create-user.spec.ts
+++ src/data/usecases/create-user/remote-create-user.spec.ts
@@ -1,5 +1,7 @@
 import { describe, test, expect } from 'vitest'
 
+import { UnexpectedError } from '@/data/errors'
+import { HttpStatusCode } from '@/data/protocols/http'
 import { HttpPostClientSpy } from '@/data/test/mock-http'
 import { mockAccountModel } from '@/domain/test/mock-create-user'
 import { faker } from '@faker-js/faker'
@@ -32,5 +34,33 @@ describe.only('RemoteCreateUser', () => {
 
     expect(httpClientSpy.url).toBe(url)
     expect(httpClientSpy.body).toEqual(account)
+  })
+
+  test('ensure RemoteCreateUser return an AccountModel on status 200', async () => {
+    const url = faker.internet.url()
+    const { httpClientSpy, sut } = makeSut(url)
+
+    const mockResponse = mockAccountModel()
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: mockResponse,
+    }
+
+    const response = await sut.create(mockAccountModel())
+
+    expect(response).toEqual(mockResponse)
+  })
+
+  test('ensure RemoteCreateUser throw error on fail', async () => {
+    const url = faker.internet.url()
+    const { httpClientSpy, sut } = makeSut(url)
+
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.badRequest,
+    }
+
+    const response = sut.create(mockAccountModel())
+
+    expect(response).rejects.toThrow(new UnexpectedError())
   })
 })
