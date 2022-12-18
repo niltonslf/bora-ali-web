803c0fbbb1e06fd4f4d7cb4accc55174e16cc7a7
niltonslf <niltonrck@gmail.com>
2022-12-18 06:56:13 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/create-user/remote-create-user.spec.ts
+++ src/data/usecases/create-user/remote-create-user.spec.ts
@@ -2,19 +2,35 @@ import { describe, test, expect } from 'vitest'
 
 import { HttpPostClientSpy } from '@/data/test/mock-http'
 import { mockAccountModel } from '@/domain/test/mock-create-user'
+import { faker } from '@faker-js/faker'
 
 import { RemoteCreateUser } from './remote-create-user'
 
+type SutTypes = {
+  httpClientSpy: HttpPostClientSpy
+  sut: RemoteCreateUser
+}
+
+const makeSut = (url = faker.internet.url()): SutTypes => {
+  const httpClientSpy = new HttpPostClientSpy()
+  const sut = new RemoteCreateUser(url, httpClientSpy)
+
+  return {
+    httpClientSpy,
+    sut,
+  }
+}
+
 describe.only('RemoteCreateUser', () => {
   test('Should call RemoteCreateUser with correct values', () => {
-    const httpClientSpy = new HttpPostClientSpy()
-    const remoteCreateUser = new RemoteCreateUser('/user', httpClientSpy)
+    const url = faker.internet.url()
+    const { httpClientSpy, sut } = makeSut(url)
 
     const account = mockAccountModel()
 
-    remoteCreateUser.create(account)
+    sut.create(account)
 
-    expect(httpClientSpy.url).toBe('/user')
+    expect(httpClientSpy.url).toBe(url)
     expect(httpClientSpy.body).toEqual(account)
   })
 })
