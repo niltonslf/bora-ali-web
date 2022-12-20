18f649d2573af862e28fdf9be67fd6b48bc1bdcd
niltonslf <niltonrck@gmail.com>
2022-12-20 09:37:45 -0300

    refactor: move sut creation to a factory method

--- src/infra/firebase/firebase-client.spec.ts
+++ src/infra/firebase/firebase-client.spec.ts
@@ -7,6 +7,15 @@ import { FirebaseClient } from './firebase-client'
 
 vi.mock('firebase/auth')
 
+type SutTypes = {
+  sut: FirebaseClient
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new FirebaseClient()
+  return { sut }
+}
+
 describe.only('FirebaseClient', () => {
   test('FirebaseClient.signIn should call firebase/auth and return user and token', async () => {
     const user = JSON.parse(faker.datatype.json())
@@ -18,7 +27,23 @@ describe.only('FirebaseClient', () => {
       accessToken,
     } as any)
 
-    const sut = new FirebaseClient()
+    const { sut } = makeSut()
+    const response = await sut.signIn()
+
+    expect(response).toEqual({ user, token: accessToken })
+  })
+
+  test('FirebaseClient.signIn should call firebase/auth and return user and token', async () => {
+    const user = JSON.parse(faker.datatype.json())
+    const accessToken = faker.datatype.uuid()
+
+    vi.spyOn(firebase, 'signInWithPopup').mockReturnValue({ user } as any)
+
+    vi.spyOn(firebase.GoogleAuthProvider, 'credentialFromResult').mockReturnValue({
+      accessToken,
+    } as any)
+
+    const { sut } = makeSut()
     const response = await sut.signIn()
 
     expect(response).toEqual({ user, token: accessToken })
