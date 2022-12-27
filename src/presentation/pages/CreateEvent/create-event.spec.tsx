66e600fb4715ab4835cdffb7dc83227818f52fe0
niltonslf <niltonrck@gmail.com>
2022-12-27 00:37:24 -0300

    refactor: move sut creation to a factory method

--- src/presentation/pages/CreateEvent/create-event.spec.tsx
+++ src/presentation/pages/CreateEvent/create-event.spec.tsx
@@ -1,4 +1,4 @@
-import { createMemoryHistory } from 'history'
+import { createMemoryHistory, MemoryHistory } from 'history'
 import { Router } from 'react-router-dom'
 import { describe, expect, test } from 'vitest'
 
@@ -6,17 +6,28 @@ import { render, screen } from '@testing-library/react'
 
 import { CreateEvent } from '.'
 
+type SutTypes = {
+  history: MemoryHistory
+}
+
+const makeSut = (): SutTypes => {
+  const history = createMemoryHistory()
+
+  render(
+    <Router location={history.location} navigator={history}>
+      <CreateEvent />
+    </Router>
+  )
+
+  return {
+    history,
+  }
+}
+
 describe('<CreateEvent/>', () => {
   describe('<EventTypePage />', () => {
     test('should load first page of event creation', () => {
-      const history = createMemoryHistory()
-
-      render(
-        <Router location={history.location} navigator={history}>
-          <CreateEvent />
-        </Router>
-      )
-
+      makeSut()
       expect(screen.getByTestId('event-type-title')).toBeInTheDocument()
     })
   })
