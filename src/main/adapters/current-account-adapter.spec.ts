b2d4400cfbbb3d2d96dc91d07957d9c9ffae824f
niltonslf <niltonrck@gmail.com>
2023-01-12 10:32:02 -0300

    test: ensure LocalStorageAdapter calls localStorage.getItem with correct value

---
+++ src/main/adapters/current-account-adapter.spec.ts
@@ -0,0 +1,42 @@
+import { describe, expect, test, vi } from 'vitest'
+
+import { UnexpectedError } from '@/data/errors'
+import { mockAccountModel } from '@/domain/test'
+import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter'
+import { faker } from '@faker-js/faker'
+
+import { getCurrentAccountAdapter, setCurrentAccountAdapter } from './current-account-adapter'
+
+vi.mock('@/infra/cache/local-storage-adapter')
+
+describe('CurrentAccountAdapter', () => {
+  describe('setCurrentAccountAdapter', () => {
+    test('should call LocalStorageAdapter.set with correct values', () => {
+      const account = mockAccountModel()
+      const accessToken = faker.datatype.uuid()
+      const setSpy = vi.spyOn(LocalStorageAdapter.prototype, 'set')
+
+      setCurrentAccountAdapter(account, accessToken)
+
+      expect(setSpy).toHaveBeenCalledWith('account', { ...account, accessToken })
+    })
+
+    test('should throw UnexpectedError ', () => {
+      expect(() => {
+        setCurrentAccountAdapter(undefined, undefined)
+      }).toThrow(new UnexpectedError())
+    })
+  })
+
+  describe('getCurrentAccountAdapter', () => {
+    test('should call LocalStorageAdapter.get with correct value', () => {
+      const account = mockAccountModel()
+      const getSpy = vi.spyOn(LocalStorageAdapter.prototype, 'get').mockReturnValue(account)
+
+      const result = getCurrentAccountAdapter()
+
+      expect(getSpy).toHaveBeenCalledWith('account')
+      expect(result).toBe(account)
+    })
+  })
+})
