affa8051211abd44c30f63b267905fb0998bd30f
niltonslf <niltonrck@gmail.com>
2023-01-13 21:31:00 -0300

    feat: add AuthorizeHttpGetClientDecorator factory

--- src/main/factories/pages/EventMap/EventMapFactory.tsx
+++ src/main/factories/pages/EventMap/EventMapFactory.tsx
@@ -1,9 +1,9 @@
 import { RemoteFetchEvent } from '@/data/usecases'
-import { makeAxiosHttpClient } from '@/main/factories/http/axios-http-client-factory'
+import { makeAuthorizeHttpGetClientDecorator } from '@/main/factories/decorators'
 import { EventMap } from '@/presentation/pages'
 
 export const EventMapFactory: React.FC = () => {
-  const axios = makeAxiosHttpClient()
+  const axios = makeAuthorizeHttpGetClientDecorator()
   const fetchEvent = new RemoteFetchEvent('/event', axios)
 
   return <EventMap fetchEvent={fetchEvent} />
