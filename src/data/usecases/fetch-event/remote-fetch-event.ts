abbd31853bc50464d123fd5fd68ab51013aeeb4d
niltonslf <niltonrck@gmail.com>
2023-01-26 19:17:12 -0300

    feat: add  fetchById method

--- src/data/usecases/fetch-event/remote-fetch-event.ts
+++ src/data/usecases/fetch-event/remote-fetch-event.ts
@@ -1,5 +1,5 @@
 import { AccessDeniedError, InvalidCredentialsError, UnexpectedError } from '@/data/errors'
-import { HttpStatusCode, HttpClient } from '@/data/protocols/http'
+import { HttpStatusCode, HttpClient, HttpResponse } from '@/data/protocols/http'
 import { EventModel } from '@/domain/models'
 import { FetchEvent } from '@/domain/usecases'
 
@@ -9,22 +9,7 @@ export class RemoteFetchEvent implements FetchEvent {
   async fetchAll(): Promise<EventModel[]> {
     const response = await this.httpClient.request({ url: this.url, method: 'get' })
 
-    switch (response.statusCode) {
-      case HttpStatusCode.ok:
-        return response.body
-
-      case HttpStatusCode.noContent:
-        return []
-
-      case HttpStatusCode.unauthorized:
-        throw new InvalidCredentialsError()
-
-      case HttpStatusCode.forbidden:
-        throw new AccessDeniedError()
-
-      default:
-        throw new UnexpectedError()
-    }
+    return this.handleResponse<EventModel[]>(response)
   }
 
   async fetchByLocation(lat: number, lng: number, radius: number): Promise<EventModel[]> {
@@ -34,12 +19,25 @@ export class RemoteFetchEvent implements FetchEvent {
       params: { lat, lng, radius },
     })
 
+    return this.handleResponse<EventModel[]>(response)
+  }
+
+  async fetchById(eventId: string): Promise<EventModel> {
+    const response = await this.httpClient.request({
+      url: `/event/location/${eventId}`,
+      method: 'get',
+    })
+
+    return this.handleResponse<EventModel>(response)
+  }
+
+  private handleResponse<Response>(response: HttpResponse): Response {
     switch (response.statusCode) {
       case HttpStatusCode.ok:
         return response.body
 
       case HttpStatusCode.noContent:
-        return []
+        return null as any
 
       case HttpStatusCode.unauthorized:
         throw new InvalidCredentialsError()
