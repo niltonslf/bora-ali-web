70be00af63ac90b753d30b3377d3eafe25fdcdf0
niltonslf <niltonrck@gmail.com>
2023-01-26 18:34:52 -0300

    test: fix tests

--- src/presentation/pages/EventMap/event-map.spec.tsx
+++ src/presentation/pages/EventMap/event-map.spec.tsx
@@ -6,9 +6,31 @@ import { describe, test, expect, vi } from 'vitest'
 import { AccessDeniedError, UnexpectedError } from '@/data/errors'
 import { FetchEventSpy } from '@/presentation/test'
 import { ThemeWrapper } from '@/presentation/test/theme-wrapper'
-import { act, render, screen } from '@testing-library/react'
+import { act, render, screen, waitFor } from '@testing-library/react'
 
 import { EventMap } from '.'
+
+global.navigator = {
+  ...global.navigator,
+  geolocation: {
+    watchPosition: vi.fn(),
+    clearWatch: vi.fn(),
+    getCurrentPosition: (position) => {
+      return position({
+        timestamp: 1,
+        coords: {
+          speed: 1,
+          heading: 1,
+          accuracy: 1,
+          altitudeAccuracy: 1,
+          altitude: 1,
+          latitude: 1,
+          longitude: 1,
+        },
+      })
+    },
+  },
+}
 
 type SutTypes = {
   fetchEventSpy: FetchEventSpy
@@ -45,8 +67,10 @@ describe('EventMap Page', () => {
 
   test('should call fetchEvent', async () => {
     const { fetchEventSpy } = makeSut()
-    const eventMap = screen.getByTestId('event-list')
-    await act(async () => eventMap)
+    const eventList = screen.getByTestId('event-list')
+    await waitFor(async () => eventList)
+    await act(async () => screen.getByTestId('title'))
+
     expect(fetchEventSpy.callsCount).toBe(1)
   })
 
@@ -64,7 +88,7 @@ describe('EventMap Page', () => {
     const fetchEventSpy = new FetchEventSpy()
     const error = new UnexpectedError()
 
-    vi.spyOn(fetchEventSpy, 'fetchAll').mockRejectedValue(error)
+    vi.spyOn(fetchEventSpy, 'fetchByLocation').mockRejectedValue(error)
     makeSut(fetchEventSpy)
 
     await act(async () => screen.getByTestId('title'))
@@ -75,7 +99,7 @@ describe('EventMap Page', () => {
 
   test('should render EventMap with error on AccessDenied', async () => {
     const fetchEventSpy = new FetchEventSpy()
-    vi.spyOn(fetchEventSpy, 'fetchAll').mockRejectedValue(new AccessDeniedError())
+    vi.spyOn(fetchEventSpy, 'fetchByLocation').mockRejectedValue(new AccessDeniedError())
 
     const { history } = makeSut(fetchEventSpy)
 
