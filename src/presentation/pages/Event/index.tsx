7faa31f3f3dd6792d8c1cf8c2e7680955e24a739
niltonslf <niltonrck@gmail.com>
2023-01-29 12:44:48 -0300

    feat: ensure event page calls fetchEventById

--- src/presentation/pages/Event/index.tsx
+++ src/presentation/pages/Event/index.tsx
@@ -1,3 +1,8 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+import { EventModel } from '@/domain/models'
+import { FetchEvent } from '@/domain/usecases'
 import { GoogleMapsLoader, Header } from '@/presentation/components'
 import { Box, Divider, Flex, Heading, List, ListItem, Text } from '@chakra-ui/react'
 import { GoogleMap, Marker } from '@react-google-maps/api'
@@ -5,43 +10,38 @@ import { GoogleMap, Marker } from '@react-google-maps/api'
 import { Gallery } from './components'
 
 type EventProps = {
-  any?: any
+  fetchEvent: FetchEvent
 }
 
-export const Event: React.FC<EventProps> = () => {
+export const Event: React.FC<EventProps> = ({ fetchEvent }) => {
+  const { eventId } = useParams()
+
+  const [event, setEvent] = useState<EventModel>(null as any)
+
+  useEffect(() => {
+    if (eventId) fetchEvent.fetchById(eventId).then((event) => setEvent(event))
+  }, [eventId])
+
+  if (!event) return <></>
+
   return (
     <Flex width='100%' flexFlow='row wrap'>
       <Header />
       <Flex flexFlow='row wrap' width='75rem' maxWidth='100%' margin='0 auto' paddingY='2rem'>
         <Box width='100%' data-testid='title-section'>
-          <Heading size='md'>Universo paralelo 2023</Heading>
+          <Heading size='md'>{event.name}</Heading>
           <Flex gap='1rem' width='100%' textStyle='label'>
-            <Text>Praia do retiro, Bahia, Brazil</Text>
-            <Text>22/01/2023 - 01/03/203</Text>
+            <Text>{event.address}</Text>
+            <Text>
+              {event.startDate} - {event.endDate}
+            </Text>
           </Flex>
         </Box>
 
         <Gallery />
 
         <Flex marginTop='1rem' data-testid='description-section'>
-          <Text>
-            Id molestiae reprehenderit ut deleniti laboriosam ut recusandae saepe et repellendus
-            sunt est rerum amet! Aut totam aliquid eum dolorem quis cum labore enim est nostrum
-            atque ea esse iusto et tempore asperiores a exercitationem inventore. Et iure sequi est
-            quaerat suscipit qui voluptatem galisum non assumenda repudiandae qui sunt ipsam ut
-            excepturi animi eos inventore quos! Et dolor dolores qui error ullam id dicta provident
-            ad facilis culpa est voluptas <br />
-            <br />
-            aliquid ut Quis molestiae aut doloribus illum. Qui nostrum corporis ut perferendis quia
-            est pariatur enim 33 quia incidunt cum repellat temporibus. Qui nemo quibusdam est quasi
-            quis et aspernatur cupiditate et provident cupiditate 33 quos mollitia et aspernatur
-            eaque et laudantium ullam. 33 molestiae optio in accusamus quam qui quos atque. Ea vero
-            mollitia ea dolores accusantium et rerum facere.
-            <br />
-            <br /> Id dolorem enim ad molestias deserunt aut sunt itaque. A minus dolorem in quaerat
-            molestiae et reprehenderit voluptatum et provident excepturi et fugiat tempora aut quia
-            facere. Qui eligendi perferendis sit voluptatum delectus in quod recusandae.
-          </Text>
+          <Text>{event.description}</Text>
         </Flex>
 
         <Divider marginY='1rem' />
@@ -69,7 +69,7 @@ export const Event: React.FC<EventProps> = () => {
             <GoogleMapsLoader>
               <GoogleMap
                 mapContainerStyle={{ width: '100%', height: '100%' }}
-                center={{ lat: -33.91519386250274, lng: 18.420095308767127 }}
+                center={{ lat: Number(event.lat), lng: Number(event.lng) }}
                 zoom={15}
                 options={{
                   fullscreenControl: false,
@@ -79,8 +79,8 @@ export const Event: React.FC<EventProps> = () => {
                 }}
               >
                 <Marker
-                  title='Event'
-                  position={{ lat: -33.91519386250274, lng: 18.420095308767127 }}
+                  title={event.name}
+                  position={{ lat: Number(event.lat), lng: Number(event.lng) }}
                 />
               </GoogleMap>
             </GoogleMapsLoader>
