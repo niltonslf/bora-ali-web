071bb64290fc6ffe7646dffd43181d33b8a2ea58
niltonslf <niltonrck@gmail.com>
2023-01-29 14:18:57 -0300

    fix: resolving some  errors

--- src/presentation/pages/Event/index.tsx
+++ src/presentation/pages/Event/index.tsx
@@ -19,7 +19,7 @@ type EventProps = {
 export const Event: React.FC<EventProps> = ({ fetchEvent }) => {
   const { eventId } = useParams()
 
-  const [, setError] = useState<string | null>(null)
+  const [error, setError] = useState<string | null>(null)
   const [event, setEvent] = useState<EventModel>(null as any)
   const [map, setMap] = useState<google.maps.Map>()
 
@@ -28,8 +28,8 @@ export const Event: React.FC<EventProps> = ({ fetchEvent }) => {
   useEffect(() => {
     if (!map) return
     const marker = new google.maps.Marker({
-      position: { lat: Number(event.lat), lng: Number(event.lng) },
-      title: event.name,
+      position: { lat: Number(event?.lat), lng: Number(event?.lng) },
+      title: event?.name,
     })
     marker.setMap(map)
 
@@ -46,75 +46,78 @@ export const Event: React.FC<EventProps> = ({ fetchEvent }) => {
         .catch((error) => handleError(error))
   }, [eventId])
 
-  if (!event) return <></>
-
   return (
     <Flex width='100%' flexFlow='row wrap' paddingX='1rem'>
       <Header />
-      <Flex flexFlow='row wrap' width='75rem' maxWidth='100%' margin='0 auto' paddingY='2rem'>
-        <Box width='100%' data-testid='title-section'>
-          <Heading size='md'>{event.name}</Heading>
 
-          <Flex gap='0.5rem' width='100%' textStyle='label' alignItems='center'>
-            <FaMapMarkerAlt />
-            <Text>{event.address}</Text>
+      {error ? (
+        <>{error}</>
+      ) : (
+        <Flex flexFlow='row wrap' width='75rem' maxWidth='100%' margin='0 auto' paddingY='2rem'>
+          <Box width='100%' data-testid='title-section'>
+            <Heading size='md'>{event?.name}</Heading>
+
+            <Flex gap='0.5rem' width='100%' textStyle='label' alignItems='center'>
+              <FaMapMarkerAlt />
+              <Text>{event?.address}</Text>
+            </Flex>
+
+            <Flex gap='0.5rem' width='100%' textStyle='label' alignItems='center'>
+              <FaCalendar />
+              <Text>
+                {formatDateToReadable(event?.startDate)} - {formatDateToReadable(event?.endDate)}
+              </Text>
+            </Flex>
+          </Box>
+
+          <Gallery images={event?.images || []} />
+
+          <Flex marginTop='1rem' data-testid='description-section'>
+            <Text>{event?.description}</Text>
           </Flex>
 
-          <Flex gap='0.5rem' width='100%' textStyle='label' alignItems='center'>
-            <FaCalendar />
-            <Text>
-              {formatDateToReadable(event.startDate)} - {formatDateToReadable(event.endDate)}
-            </Text>
+          <Divider marginY='1rem' />
+
+          <Flex flexFlow='row wrap' data-testid='options-section'>
+            <Heading size='md' width='100%' marginBottom='1rem'>
+              What you are going to find here
+            </Heading>
+
+            <List>
+              <ListItem>Pool</ListItem>
+              <ListItem>Free drink</ListItem>
+              <ListItem>Beach</ListItem>
+              <ListItem>Open Air</ListItem>
+              <ListItem>Music</ListItem>
+            </List>
           </Flex>
-        </Box>
 
-        <Gallery images={event.images} />
+          <Divider marginY='1rem' />
 
-        <Flex marginTop='1rem' data-testid='description-section'>
-          <Text>{event.description}</Text>
-        </Flex>
+          <Flex flexFlow='row wrap' width='100%' data-testid='map-section'>
+            <Heading size='md' width='100%' marginBottom='1rem'>
+              Where will be
+            </Heading>
 
-        <Divider marginY='1rem' />
-
-        <Flex flexFlow='row wrap' data-testid='options-section'>
-          <Heading size='md' width='100%' marginBottom='1rem'>
-            What you are going to find here
-          </Heading>
-
-          <List>
-            <ListItem>Pool</ListItem>
-            <ListItem>Free drink</ListItem>
-            <ListItem>Beach</ListItem>
-            <ListItem>Open Air</ListItem>
-            <ListItem>Music</ListItem>
-          </List>
-        </Flex>
-
-        <Divider marginY='1rem' />
-
-        <Flex flexFlow='row wrap' width='100%' data-testid='map-section'>
-          <Heading size='md' width='100%' marginBottom='1rem'>
-            Where will be
-          </Heading>
-
-          <Flex width='100%' height='20rem'>
-            <GoogleMapsLoader>
-              <GoogleMap
-                mapContainerStyle={{ width: '100%', height: '100%' }}
-                center={{ lat: Number(event.lat), lng: Number(event.lng) }}
-                onLoad={setMap}
-                zoom={15}
-                options={{
-                  fullscreenControl: false,
-                  mapTypeControl: false,
-                  streetViewControl: false,
-                  zoomControl: false,
-                }}
-              ></GoogleMap>
-            </GoogleMapsLoader>
+            <Flex width='100%' height='20rem'>
+              <GoogleMapsLoader>
+                <GoogleMap
+                  mapContainerStyle={{ width: '100%', height: '100%' }}
+                  center={{ lat: Number(event?.lat) || 0, lng: Number(event?.lng) || 0 }}
+                  onLoad={setMap}
+                  zoom={15}
+                  options={{
+                    fullscreenControl: false,
+                    mapTypeControl: false,
+                    streetViewControl: false,
+                    zoomControl: false,
+                  }}
+                />
+              </GoogleMapsLoader>
+            </Flex>
           </Flex>
         </Flex>
-      </Flex>
+      )}
     </Flex>
   )
 }
