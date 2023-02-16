e0ea74f40830012269c3d26f33685bddf02bd3ed
niltonslf <niltonrck@gmail.com>
2023-02-16 17:50:06 -0300

    fix: date

--- src/presentation/utils/date.ts
+++ src/presentation/utils/date.ts
@@ -9,9 +9,10 @@ export const formatDateFromBrToDb = (date: string): string => {
 export const formatDateToReadable = (dateTime: string): string => {
   if (!dateTime) return ''
 
-  const date = new Date(dateTime)
+  const [date] = dateTime.split('T')
+  const newDate = new Date(`${date} 00:00:00`)
 
-  return `${date.toLocaleDateString('pt-BR')}`
+  return `${newDate.toLocaleDateString('pt-BR')}`
 }
 export const formatTimeToReadable = (time: string): string => {
   if (!time) return ''
