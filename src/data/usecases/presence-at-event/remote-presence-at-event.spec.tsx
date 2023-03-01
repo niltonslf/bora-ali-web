437c741015ec800dee1a4c833246051970f8e3fd
niltonslf <niltonrck@gmail.com>
2023-03-01 17:31:10 -0300

    test: ensure RemotePresenceAtEvent confirm presence with success and failure

--- src/data/usecases/presence-at-event/remote-presence-at-event.spec.tsx
+++ src/data/usecases/presence-at-event/remote-presence-at-event.spec.tsx
@@ -1,5 +1,7 @@
 import { describe, test, expect } from 'vitest'
 
+import { UnexpectedError } from '@/data/errors'
+import { HttpStatusCode } from '@/data/protocols/http'
 import { HttpClientSpy } from '@/data/test'
 import { faker } from '@faker-js/faker'
 
@@ -32,6 +34,38 @@ describe('RemotePresenceAtEvent', () => {
 
       expect(httpClient.url).toBe(`/event/${eventId}/confirm-presence`)
       expect(httpClient.body).toEqual({ userId })
+    })
+
+    test('Ensure to  confirm presence with success', async () => {
+      const { sut, httpClient } = makeSut()
+
+      const eventId = faker.datatype.uuid()
+      const userId = faker.datatype.uuid()
+
+      httpClient.response = {
+        statusCode: HttpStatusCode.ok,
+        body: null,
+      }
+
+      const response = await sut.confirm(eventId, userId)
+
+      expect(response).toBe(null)
+    })
+
+    test('Ensure to confirm presence with failure', async () => {
+      const { sut, httpClient } = makeSut()
+
+      const eventId = faker.datatype.uuid()
+      const userId = faker.datatype.uuid()
+
+      httpClient.response = {
+        statusCode: HttpStatusCode.serverError,
+        body: null,
+      }
+
+      const response = sut.confirm(eventId, userId)
+
+      expect(response).rejects.toThrow(new UnexpectedError())
     })
   })
 
