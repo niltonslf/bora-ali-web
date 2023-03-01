c6f6c400f84202ddb7c804db7fdf9efc5e85d416
niltonslf <niltonrck@gmail.com>
2023-03-01 13:00:46 -0300

    feat: ensure confirm presence calls HttpClient with all params

---
+++ src/data/usecases/presence-at-event/remote-presence-at-event.ts
@@ -0,0 +1,29 @@
+import { UnexpectedError } from '@/data/errors'
+import { HttpClient, HttpStatusCode } from '@/data/protocols/http'
+import { PresenceAtEvent } from '@/domain/usecases'
+
+export class RemotePresenceAtEvent implements PresenceAtEvent {
+  constructor(private readonly httpClient: HttpClient) {}
+
+  async confirm(eventId: string, userId: string): Promise<any> {
+    const response = await this.httpClient.request({
+      method: 'get',
+      url: `/event/${eventId}/confirm-presence`,
+      body: {
+        userId,
+      },
+    })
+
+    switch (response.statusCode) {
+      case HttpStatusCode.ok:
+        return response?.body || null
+
+      default:
+        throw new UnexpectedError()
+    }
+  }
+
+  async cancel(eventId: string, userId: string): Promise<any> {
+    //
+  }
+}
